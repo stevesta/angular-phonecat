b390fb00bb70a7c6b7b5a6ea87da5c49447ebf62
steve s <ses1984@gmail.com>
2014-12-09 12:31:40 -0500

    Testing out custom filter in ng-repeat directive

--- app/js/filters.js
+++ app/js/filters.js
@@ -6,4 +6,13 @@ angular.module('phonecatFilters', []).filter('checkmark', function() {
   return function(input) {
     return input ? '\u2713' : '\u2718';
   };
+}).filter('group_by_two', function() {
+ return function(input) {
+     var input_by_two = [];
+     var input_length = input.length;
+     for (var i=0; i < input_length; i=i+2) {
+             input_by_two.push([input[i],input[i+1]]);
+    }
+   return input_by_two;
+  };
 });
